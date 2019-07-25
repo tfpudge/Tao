2a07ff7c93e3f7d1921fcbc31992ed235b04aaea
Tao <2269105443@qq.com>
2019-07-25 15:28:53 +0800

    main方法增加引用类，新建test文件

--- OOTest.java
+++ OOTest.java
@@ -1,6 +1,8 @@
 public class OOTest{
 	public static void main(String[] args){
-		Student s;
+		Student s = new Student();
+		String stu = "这是str引用";
+		System.out.println(s.name);
 	}
 }
 class Student{
